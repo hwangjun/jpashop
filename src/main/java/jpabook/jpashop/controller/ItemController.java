147431e2a710e6743600dd0dc86738d95bcbf03e
hwangjun9349@gmail.com <hwangjun@hwangjun-ui-MacBookPro.local>
2022-06-21 19:06:47 +0900

    jpa 활용1 완료

---
+++ src/main/java/jpabook/jpashop/controller/ItemController.java
@@ -0,0 +1,71 @@
+package jpabook.jpashop.controller;
+
+import com.sun.org.apache.xpath.internal.operations.Mod;
+import jpabook.jpashop.domain.item.Book;
+import jpabook.jpashop.domain.item.Item;
+import jpabook.jpashop.service.ItemService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.validation.Valid;
+import java.util.List;
+
+@Controller
+@RequiredArgsConstructor
+public class ItemController {
+
+    private final ItemService itemService;
+
+    @GetMapping("/items/new")
+    public String createBook(Model model) {
+        model.addAttribute("form", new BookForm());
+        return "items/createItemForm";
+    }
+
+    @PostMapping("/items/new")
+    public String create(BookForm bookForm) {
+        Book book = new Book();
+        book.setName(bookForm.getName());
+        book.setIsbn(bookForm.getIsbn());
+        book.setAuthor(bookForm.getAuthor());
+        book.setPrice(bookForm.getPrice());
+        book.setStockQuantity(bookForm.getStockQuantity());
+
+        itemService.saveItem(book);
+        return "redirect:/items";
+    }
+
+    @GetMapping("/items")
+    public String list(Model model) {
+        List<Item> items = itemService.findItems();
+        model.addAttribute("items", items);
+        return "items/itemList";
+    }
+
+    @GetMapping("/items/{itemId}/edit")
+    public String edit(@PathVariable Long itemId, Model model) {
+        Book item = (Book) itemService.findOne(itemId);
+
+        Book book = new Book();
+        book.setName(item.getName());
+        book.setPrice(item.getPrice());
+        book.setStockQuantity(item.getStockQuantity());
+        book.setAuthor(item.getAuthor());
+        book.setIsbn(item.getIsbn());
+
+        model.addAttribute("form", book);
+        return "items/updateItemForm";
+    }
+
+    @PostMapping("/items/{itemId}/edit")
+    public String updateItem(@PathVariable Long itemId, @ModelAttribute("form") BookForm form) {
+        itemService.updateItem(itemId, form.getName(), form.getPrice(), form.getStockQuantity());
+        return "redirect:/items";
+    }
+}
--- src/main/java/jpabook/jpashop/domain/item/Item.java
+++ src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -4,19 +4,20 @@ import jpabook.jpashop.domain.Category;
 import jpabook.jpashop.domain.exception.NotEnoughStockException;
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.annotations.BatchSize;
 
 import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
 
 @Entity
-@Inheritance(strategy = InheritanceType.SINGLE_TABLE) // 한테이블에 다 들어감
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
 @DiscriminatorColumn(name = "dtype")
 @Getter @Setter
 public abstract class Item {
-    //상속관계 맵핑
 
-    @Id @GeneratedValue
+    @Id
+    @GeneratedValue
     @Column(name = "item_id")
     private Long id;
 
@@ -30,7 +31,6 @@ public abstract class Item {
     //==비즈니스 로직==//
     /**
      * stock 증가
-     * @param quantity
      */
     public void addStock(int quantity) {
         this.stockQuantity += quantity;
@@ -38,11 +38,10 @@ public abstract class Item {
 
     /**
      * stock 감소
-     * @param quantity
      */
     public void removeStock(int quantity) {
         int restStock = this.stockQuantity - quantity;
-        if(restStock < 0) {
+        if (restStock < 0) {
             throw new NotEnoughStockException("need more stock");
         }
         this.stockQuantity = restStock;
